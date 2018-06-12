06c425af83eeb57fe9d98954bc5d3ae98853e2ea
Peter van Dijk <35191354+padvd@users.noreply.github.com>
2018-06-12 17:18:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World : CHANGED ON GitHub + NOW ON PETER-PC\n');
+  res.end('Hello World : CHANGED ON GitHub + NOW ON PETER-PC ABCDE\n');
 });
 
 server.listen(port, hostname, () => {
